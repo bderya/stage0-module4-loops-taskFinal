cd1e5357e6aa27f9aad6b1c4ef227b4c5aa5b984
Batuhan Derya <bderya@3-G-9.42heilbronn.de>
2022-09-26 00:55:56 +0200

    Delir

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,26 +3,24 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) 
 	{
-        for (int i = 1; i <= cathetusLength; i++) 
+        for (int i = 1; i <= cathetusLength; i++)
 		{
-			for (int j = 0; j < cathetusLength - i; j++)
+
+            for (int j = 1; j <= cathetusLength - i; j++)
 			{
-				System.out.print(" ");
-			}
-			for (int k = i; k >= 1; k--)
+                System.out.print(" ");
+            }
+            for (int k = i; k >= 1; k--)
 			{
-				System.out.print(k);
-			}
-			for (int l = 2; l <= i; l++)
+                System.out.print(k);
+            }
+            for (int k = 2; k <= i; k++) 
 			{
-				System.out.print(l);
-			}
-			for(int d = cathetusLength - i; d >= 1; d--)
-			{
-				System.out.print(" ");
-			}
-			System.out.println();
-		}
+                System.out.print(k);
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
